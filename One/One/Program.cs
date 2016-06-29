678be166e25c783ddadff3b77d2155accfbf9dd0
John Reese <jpreese@mtu.edu>
2016-06-29 19:01:43 -0400

    Initial commit

---
+++ One/One/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = Enumerable.Range(0, 1000).Where(n => n % 3 == 0 || n % 5 == 0).Sum();
+            Debug.WriteLine(sum);
+        }
+    }
+}
